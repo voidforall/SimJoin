38e05967e2725741fa5eb26806b15e9356c8148f
lynn <voidforall@gmail.com>
2021-05-02 04:14:39 +0200

    Optimize clusterjoin with preventing duplicated verification

--- src/main/scala/joinop/ClusterJoin.scala
+++ src/main/scala/joinop/ClusterJoin.scala
@@ -1,82 +1,78 @@
 package joinop
 
 import java.util.Calendar
-import scala.collection.mutable.ListBuffer
 import org.apache.spark.rdd._
 import org.apache.spark.HashPartitioner
 import utils.Distance
 
-
 class ClusterJoin(measure: String, threshold: Double, anchorNum: Int) extends Serializable{
-  protected val measureObj = new Distance()
+  var measureObj = new Distance()
 
-  // Approximate outer partition membership
-  // Ref: paper 6.2 General filter for any metric distance
-  def generalFilter(query: String, center: String, test: String): Boolean = {
-    if(measureObj.editDistance(query, test) > measureObj.editDistance(query, center) + 2*threshold)
-      false
-    else
-      true
+  // Execute local join (verification) on each partition, performance depending on the parallelism
+  def localJoin(cluster: Iterator[(Int, (String, (Int, String)))]): Iterator[((Int, String), (Int, String))] = {
+    var result = List[((Int, String), (Int, String))]()
+    var homePoints = List[(Int, String)]()
+
+    while (cluster.hasNext) {
+      val point = cluster.next
+      for(prevPoint <- homePoints){
+        // Increasing id and length filtering
+        if( (point._2._2._2.length - prevPoint._2.length) <= threshold)
+//        if(point._2._2._1 > prevPoint._1 && (point._2._2._2.length - prevPoint._2.length) <= threshold){
+          // Call the metric function to verify the pair
+          if(measureObj.editDistance(prevPoint._2, point._2._2._2) <= threshold)
+            result = result :+ (prevPoint, point._2._2)
+      }
+      if(point._2._1 == "home"){
+        homePoints = homePoints :+ point._2._2
+      }
+    }
+
+    result.iterator
   }
 
   def selfJoin(table: RDD[(Int, String)]): RDD[((Int, String), (Int, String))] = {
-    // Randomly select the anchor points, given the sampling fraction
-    val anchorProb = anchorNum / table.count().toDouble
-    val anchors: RDD[(Int, String)] = table.sample(withReplacement=false, fraction=anchorProb, seed=42)
+    // Randomly select the anchor points
+    val anchors: Array[((Int, String), Int)] = table.takeSample(withReplacement = false, num = anchorNum).zipWithIndex
 
-    // Calculate the distance between all points and the anchors
-    val anchorDist: RDD[(Int, ((Int, String), (Int, String), Int))] = table.cartesian(anchors).map{
-      case (point, anchor) =>
-        (point._1, (point, anchor, measureObj.editDistance(point._2, anchor._2)))
+    // TODO: [Optimize Load Balancing] Estimate overloaded partition, and split it
+
+    // anchorDist: Calculate the distance between all points and the anchors
+    // RDD[(point, (minDist, minAnchorId))], for all the points
+    val anchorDist: RDD[((Int, String), (Int, Int))] = table.map{
+      case point@(pointId, pointVal) => (
+        point,
+        anchors.map{case anchor@(anchorPoint, anchorId) => measureObj.editDistance(pointVal, anchorPoint._2)}.zipWithIndex.min
+      )
     }
 
-    // Assign the points to one cluster center, i.e., home partition
-    val homePartition: RDD[((Int, String), (Int, String), Int)] = anchorDist.reduceByKey{
-      case (point1, point2) =>
-        if(point1._3 < point2._3) point1 else point2
-    }.map{
-      x => x._2
-    }
-
-    // Approximate outer partition membership
-    val outerPartition: RDD[((Int, String), (Int, String))] = homePartition.cartesian(anchors).filter {
-      case (assigned, anchor) => generalFilter(assigned._1._2, assigned._2._2, anchor._2)
-    }.map{
-      case (assigned, anchor) => (anchor, assigned._1)
-    }
-
-    // RDD partitioning
-    val partitioning = outerPartition.partitionBy(new HashPartitioner(anchorNum))
-
-    // In-partition (outer partition) verification by anchor points
-    val verified_pairs: RDD[((Int, String), (Int, String))] = partitioning.mapPartitions(x =>
-    {
-      // find the similar pairs in each cluster
-      var result: ListBuffer[((Int, String), (Int, String))] = ListBuffer.empty
-      var inner: ListBuffer[(Int, String)] = ListBuffer.empty
-
-      while(x.hasNext)
-      {
-        val pair = x.next()
-        val currentPoint = pair._2._2
-        for(joinPoint <- inner)
-        {
-          if(measureObj.editDistance(joinPoint._2, currentPoint) <= threshold)
-            result.append((joinPoint, pair._2))
+    // partitionMapper: Map the points to their corresponding home partition and outer partitions
+    // Note: AVOID unnecessary comparisons, see paper 7.2
+    val partitionMapper = anchorDist.flatMap{
+      case row@(point, (minDist, minAnchorId)) =>
+        anchors.map{
+          case anchor@(anchorPoint, anchorId) =>
+            if (anchorId == minAnchorId)
+              (anchorId, ("home", point))
+            else if (((minAnchorId < anchorId) ^ (minAnchorId + anchorId) % 2 == 1) &&
+                (measureObj.editDistance(point._2, anchorPoint._2) <= minDist + (2 * threshold).toInt))
+              (anchorId, ("outer", point))
+            else null
         }
-        inner.append(pair._2)
+    }
+      .filter(x => x != null)
+      .sortBy(x => (x._1, x._2._1), ascending = true)
+
+    // Hash partitioning, by anchor index
+    val partitions = partitionMapper.partitionBy(new HashPartitioner(anchorNum))
+
+    // Execute local join (verification) on each partition, performance depending on the parallelism
+    val verifiedPairs = partitions.mapPartitionsWithIndex(
+      (index, partition) => {
+        localJoin(partition)
       }
-      result.iterator
-    }).repartition(1)
+    ).repartition(1)
 
-    // Format the output
-    val output_pairs: RDD[((Int, String), (Int, String))] = verified_pairs
-      .filter(x => x._1._1 != x._2._1)
-      .map(x => if (x._1._1 < x._2._1) x else (x._2, x._1))
-      .sortByKey()
-      .distinct()
-
-    output_pairs
+    verifiedPairs
   }
-
 }
